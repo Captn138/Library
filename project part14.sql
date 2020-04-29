bffd53db779e374a56c00723409c9c14267817d3
Mikka138 <mickael.almeida1308@gmail.com>
2020-04-29 18:40:18 +0200

    v0.2
    updatet p14

--- project part14.sql
+++ project part14.sql
@@ -1,30 +1,15 @@
 --QUESTION 14--
 --Créer la fonction--
-CREATE OR REPLACE FUNCTION fct_mesureActivite(startdate IN date, duree IN number) RETURN number IS
-    enddate date;
-    resultat number;
-    createdate date;
-    membreId number;
-    CURSOR Membres1 IS
-        SELECT numero, 0 AS nbEmprunts FROM Membres;
-    --Membres2 Membres1%ROWTYPE
+CREATE OR REPLACE FUNCTION fct_mesureActivite(f_period in number)RETURN number AS memberId number;
 BEGIN
-    OPEN Membres1;
-    enddate := add_months(startdate, duree);
-    FOR elem IN (SELECT * FROM Detailsemprunts) LOOP
-        SELECT creele INTO createdate FROM Emprunts WHERE numero = elem.emprunt;
-        IF createdate > startdate AND createdate <= enddate THEN
-            SELECT membre INTO membreId FROM Emprunts WHERE numero = elem.emprunt;
-            UPDTAE Membres1
-                SET nbEmprunts = nbEmprunts + 1
-                WHERE numero = membreId;
-        END IF;
-    END LOOP;
-    SELECT numero INTO resultat FROM Membres1 ORDER BY nbEmprunts DESC WHERE ROWNUM <= 1;
-    CLOSE Membres1;
-    RETURN resultat;
+    SELECT (
+        SELECT Membre FROM (
+            SELECT Membre, count() FROM Emprunts
+            WHERE creele >= ADD_MONTHS(sysdate, - f_period)
+            GROUP BY Membre
+            ORDER BY count() DESC)
+        WHERE rownum=1)
+    INTO memberId FROM dual;
+    return memberId;
 END;
 /
-
-
---?????
